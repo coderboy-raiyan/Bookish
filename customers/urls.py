76edc503e1d9a3882d26f70c0024b4ccfb2a68ef
Raiyan <tajkierhaque@gmail.com>
2023-12-30 11:21:28 +0600

    user profile view done

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import UserSignUpView, UserSignInView
+from .views import UserSignUpView, UserSignInView, UserLogoutView, UserProfileView
 
 urlpatterns = [
     path("signup/", UserSignUpView.as_view(), name="signup"),
     path("signin/", UserSignInView.as_view(), name="signin"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("logout/", UserLogoutView.as_view(), name="logout")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from .forms import UserSignUpForm
+from .forms import UserSignUpForm, UserUpdateForm
 from django.contrib import messages
 from django.contrib.auth import login
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import View
 
 
 class UserSignUpView(FormView):
@@ -25,3 +26,25 @@ class UserSignInView(LoginView):
     def get_success_url(self):
         messages.success(self.request, "Signed in successfully")
         return reverse_lazy("home")
+
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        messages.success(self.request, "Logged out successfully")
+        return reverse_lazy("home")
+
+
+class UserProfileView(View):
+    template_name = "customers/profile.html"
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(self.request, "Profile updated Successfully")
+            return redirect("profile")
+        return render(request, self.template_name, {"form": form})
